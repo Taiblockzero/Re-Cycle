e63e65901a82315415d61bc0cd1d0ee69fe715d3
Taiblockzero <taiblockzero@gmail.com>
2024-06-15 15:29:10 +0300

    Added the scene with the monkey riding the bicycle in the town and the Player scene(the monkey)

---
+++ Scenes/player.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+@export var speed = 400 #player movespeed
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$Monkey.play()
+	else:
+		$Monkey.stop()
+	
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	if velocity.x != 0:
+		$Monkey.animation = "walking"
+		$Monkey.flip_v = false
+		$Monkey.flip_h = velocity.x < 0
