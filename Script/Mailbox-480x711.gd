6111df00238d9dfea57bc4eb7fe65f205a3096af
nicky zemenski <zemen12345@gmail.com>
2024-06-16 05:08:35 -0400

    mailbox linked to inventory which is fked up :)

--- Script/Mailbox-480x711.gd
+++ Script/Mailbox-480x711.gd
@@ -1,19 +1,21 @@
 extends Sprite2D
 
-
+var isInMail = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$mailboxHitbox/Control.hide()
-
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_key_pressed(KEY_E) && isInMail:
+		get_tree().change_scene_to_file("res://Scenes/inventory.tscn")
 
 
 func _on_mailbox_hitbox_body_entered(body):
 	$mailboxHitbox/Control.show()
-
+	isInMail = true
 
 func _on_mailbox_hitbox_body_exited(body):
 	$mailboxHitbox/Control.hide()
+	isInMail = false
